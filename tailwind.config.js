3705a1c30cd2cd2771d98738c63d5f2faa68696d
luismeza8 <angel841bv@gmail.com>
2024-10-12 18:18:41 -0700

    fuentes agregadas

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        jetbrains: ['JetBrains Mono', 'monospace'],
+      },
+    },
   },
   plugins: [],
 }
